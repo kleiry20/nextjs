1b21fac23b6b280036676f0bac13fecad2ce3111
kleiry20 <74005559+kleiry20@users.noreply.github.com>
2024-08-02 01:08:41 +0530

    working on routing

--- app/page.tsx
+++ app/page.tsx
@@ -38,13 +38,14 @@ export default function Page() {
             className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop version"
           />
-          {/* <Image
+          <Image
             src="/hero-mobile.png"
             width={560}
             height={620}
-            className="hidden sm:block"
+            className="sm:block md:hidden"
             alt="Screenshots of the dashboard project showing mobile version"
-          /> */}
+          />
+          
         </div>
       </div>
     </main>
